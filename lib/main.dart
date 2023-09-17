056501aaed1ab59e25e7d1e316ef5581c750d385
Rokia Darwish <94113862+Rokia-DSC@users.noreply.github.com>
2023-09-18 00:54:25 +0300

    'code'

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,108 @@ class AudioPage extends StatefulWidget {
 }
 
 class _AudioPageState extends State<AudioPage> {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(
+      AssetSource(
+        'note$soundNumber.wav',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              final player = AudioPlayer();
-              await player.play(
-                AssetSource(
-                  'note1.wav',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
                 ),
-              );
-            },
-            child: const Text('Click Me'),
-          ),
+                onPressed: () {
+                  playSound(1);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.purpleAccent,
+                ),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.grey,
+                ),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.greenAccent,
+                ),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: const Text('Click Me'),
+              ),
+            ),
+          ],
         ),
       ),
     );
